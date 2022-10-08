75dbe4dedb61b84f82b84dc76390a5fbc119c303
Robert Kottelin <robert.kottelin@gmail.com>
2022-10-08 14:24:57 +0200

    Update lib.rs

--- programs/mysolanaapp/src/lib.rs
+++ programs/mysolanaapp/src/lib.rs
@@ -3,42 +3,45 @@ use anchor_lang::solana_program::entrypoint::ProgramResult;
 
 declare_id!("DRq9E8WKwJsrsvhjVMQkn3888BgcPdBeHdsT5SNZq66F");
 
+
 #[program]
 mod mysolanaapp {
     use super::*;
 
-    pub fn create(ctx: Context<Create>) -> ProgramResult {
+    pub fn initialize(ctx: Context<Initialize>, data: String) -> ProgramResult {
         let base_account = &mut ctx.accounts.base_account;
-        base_account.count = 0;
+        let copy = data.clone();
+        base_account.data = data;
+        base_account.data_list.push(copy);
         Ok(())
     }
 
-    pub fn increment(ctx: Context<Increment>) -> ProgramResult {
+    pub fn update(ctx: Context<Update>, data: String) -> ProgramResult {
         let base_account = &mut ctx.accounts.base_account;
-        base_account.count += 1;
+        let copy = data.clone();
+        base_account.data = data;
+        base_account.data_list.push(copy);
         Ok(())
     }
 }
 
-// Transaction instructions
 #[derive(Accounts)]
-pub struct Create<'info> {
-    #[account(init, payer = user, space = 16 + 16)]
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 64 + 64)]
     pub base_account: Account<'info, BaseAccount>,
     #[account(mut)]
     pub user: Signer<'info>,
-    pub system_program: Program <'info, System>,
+    pub system_program: Program<'info, System>,
 }
 
-// Transaction instructions
 #[derive(Accounts)]
-pub struct Increment<'info> {
+pub struct Update<'info> {
     #[account(mut)]
     pub base_account: Account<'info, BaseAccount>,
 }
 
-// An account that goes inside a transaction instruction
 #[account]
 pub struct BaseAccount {
-    pub count: u64,
+    pub data: String,
+    pub data_list: Vec<String>,
 }
